9343b477d8c3cdc4c1b98676033c8c1439ebe6b0
manoj4089 <manoj27sky@gmail.com>
2026-07-25 22:43:38 +0530

    Remove login requirement for news details pages
    Allow visitors to view article details without signing in via Clerk.

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/news(.*)"]);
-
-export default clerkMiddleware(async (auth, request) => {
-  if (isProtectedRoute(request)) {
-    await auth.protect();
-  }
-});
+export default clerkMiddleware(async () => {});
 
 export const config = {
   matcher: [
